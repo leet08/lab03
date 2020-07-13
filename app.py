355f7c9184b175516b3ce351384d019ffc13b7f9
leet08 <leet08@gmail.com>
2020-07-13 01:56:27 -0400

    lab 3

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from models.user import Db, User
 from userform import UserForm
 
+from flask_heroku import Heroku
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/usersdb'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/usersdb'
+
+heroku = Heroku(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "s14a-key"
 Db.init_app(app)
